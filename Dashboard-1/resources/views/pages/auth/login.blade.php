1d77952f4349729604a8183a6bcba077d08d8ebc
EL Ghifari <ghifariakun@gmail.com>
2024-05-21 17:18:05 +0700

    Ubah Responsife

--- Dashboard-1/resources/views/pages/auth/login.blade.php
+++ Dashboard-1/resources/views/pages/auth/login.blade.php
@@ -127,7 +127,7 @@
                     <div class="absolute-bottom-left index-2">
                         <div class="text-light p-5 pb-2">
                             <div class="mb-5 pb-3">
-                                <h1 class="display-4 font-weight-bold mb-2">Assalamu'alaikum</h1>
+                                <h1 class="display-4 font-weight-bold mb-2" style="font-size: calc(1.5em + 1vw);">Assalamu'alaikum</h1>
                                 <h5 class="font-weight-normal text-muted-transparent">Bali, Indonesia</h5>
                             </div>
                             Photo by <a class="text-light bb"
