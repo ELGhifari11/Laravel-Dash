1aee8e6eef376f6a5043c32f20077b17fe805307
EL Ghifari <ghifariakun@gmail.com>
2024-07-03 22:50:11 +0700

    Web Route

--- Dashboard-1/routes/web.php
+++ Dashboard-1/routes/web.php
@@ -5,16 +5,16 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::middleware('auth')->group(function () {
-    Route::get('/home', function () {
-        return view('pages.dashboard');
-    })->name('home');
-});
-Route::middleware('auth')->group(function () {
-    Route::get('/', function () {
-        return view('pages.dashboard');
-    });
-});
+// Route::middleware('auth')->group(function () {
+//     Route::get('/home', function () {
+//         return view('pages.dashboard');
+//     })->name('home');
+// });
+// Route::middleware('auth')->group(function () {
+//     Route::get('/', function () {
+//         return view('pages.dashboard');
+//     });
+// });
 
 // Route::get('/login', function () {
 //     return view('pages.auth.login');
@@ -25,3 +25,11 @@ Route::middleware('auth')->group(function () {
 // Route::get('/users', function () {
 //     return view('pages.users.index');
 // });
+
+Route::redirect('/', '/dashboard');
+
+
+// Dashboard
+Route::get('/dashboard', function () {
+    return view('pages.dashboard', ['type_menu' => 'dashboard']);
+});
